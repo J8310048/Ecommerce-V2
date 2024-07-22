448dfec907ddd6a2ae0cb674f3d7f9a063756ce7
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2024-07-22 15:36:14 -0400

    edited server.js file

--- ecommerce-v2-vite/server.js
+++ ecommerce-v2-vite/server.js
@@ -27,42 +27,8 @@ connection.connect(function (err) {
 
 app.use(cors());
 
-app.get("/movies", (req, res) => {
+app.get("/", (req, res) => {
   connection.query("SELECT * FROM Movies", (error, rows) => {
-    if (error) {
-      console.error("Error retrieving information", error);
-      res.status(500).json({ error: "Error retrieving information" });
-      return;
-    }
-    res.json(rows);
-  });
-});
-
-app.get("/clips", (req, res) => {
-  connection.query("SELECT video_path FROM movie_carousel", (error, rows) => {
-    if (error) {
-      console.error("Error retrieving information", error);
-      res.status(500).json({ error: "Error retrieving information" });
-      return;
-    }
-    res.json(rows);
-  });
-});
-
-app.get('/video/:filename', (req, res) => {
-  const { filename } = req.params;
-  const filePath = path.join(__dirname, 'CarouselClips', filename); // 'CarouselClips' is where my videos are stored
-
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      console.error('Error sending file:', err);
-      res.status(404).send('File not found');
-    }
-  });
-});
-
-app.post("/contact", (req, res) => {
-  connection.query("INSERT video_path FROM movie_carousel", (error, rows) => {
     if (error) {
       console.error("Error retrieving information", error);
       res.status(500).json({ error: "Error retrieving information" });
