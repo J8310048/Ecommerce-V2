ea5e974399188a4414966378b8def62017dcfe89
Joshua Brito-Infante <the.business.of.joshua@gmail.com>
2024-07-24 00:00:37 -0400

    more design changes

--- ecommerce-v2-vite/page_components/Home.jsx
+++ ecommerce-v2-vite/page_components/Home.jsx
@@ -3,27 +3,44 @@ import Footer from "../global_components/Footer";
 import MovieCarousel from "../global_components/MovieCarousel";
 function Home() {
   return (
-    <html className="min-h-screen ">
+    <html className="min-h-screen">
       <section>
       <NavBar />
       <MovieCarousel />
       <div className="bg-blue-950  text-center text-white">
-        <article>
-        <h1>Welcome to Laser Focus!</h1>
-            <p>The premier destination for LaserDisc collectors and enthusiasts. Our passion for the classic, the rare, and the nostalgic is at the heart of everything we do. We believe that every LaserDisc tells a story, and we are here to help you find those rare gems that complete your collection.</p>
+        <article className="phonemax:px-16 grid grid-flow-row grid-cols-1 gap-5 mx-10 phonemax:grid-rows-8 justify-center items-center tabletmax:grid-cols-2 tabletmax:grid-rows-4 ">
+          <div> 
+            <h1 className="phonemin:py-10 text-4xl tabletmin:text-6xl">Welcome to Laser Focus!</h1>
+            <p className="phonemin:text-m tabletmin:text-2xl">The premier destination for LaserDisc collectors and enthusiasts. Our passion for the classic, the rare, and the nostalgic is at the heart of everything we do. We believe that every LaserDisc tells a story, and we are here to help you find those rare gems that complete your collection.</p>
+            </div>
 
+            <img src="/public/LaserDisc-Homepage-Photos/laserdisc-at-home.png" alt="LaserDisc Photo" className="rounded-3xl"/>
 
-          <h2>Why We Exist</h2>
-            <p>In a world where digital streaming dominates, we are dedicated to preserving the unique charm and tangible experience of LaserDiscs. Our mission is to provide collectors with a curated selection of rare and hard-to-find titles, ensuring that the legacy of this iconic format lives on.</p>
+            <img src="/public/LaserDisc-Homepage-Photos/laserdisc-player1.png" alt="LaserDisc Photo" className="rounded-3xl"/>
 
-          <h2>Our Commitment</h2>
-            <p>We understand the thrill of the hunt and the joy of discovery. That's why we meticulously source our inventory, guaranteeing that every item meets the highest standards of quality and authenticity. Whether you're a seasoned collector or new to the world of LaserDiscs, we offer a diverse range of titles that cater to all tastes and interests.</p>
+          <div>
+            <h2 className="phonemin:py-10 text-4xl tabletmin:text-6xl">Why We Exist</h2>
+            <p className="phonemin:text-m tabletmin:text-2xl">In a world where digital streaming dominates, we are dedicated to preserving the unique charm and tangible experience of LaserDiscs. Our mission is to provide collectors with a curated selection of rare and hard-to-find titles, ensuring that the legacy of this iconic format lives on.</p>
+            </div>
+            
 
-          <h2>Join Our Community</h2>
-            <p>At <span>Laser Focus</span>, we are more than just an online store; we are a community of like-minded individuals who share a deep appreciation for the artistry and nostalgia of LaserDiscs. Connect with us on social media, join our mailing list for exclusive updates, and become part of a vibrant community that celebrates the beauty of collecting rare items.</p>
+          <div>
+            <h2 className="phonemin:py-10 text-4xl tabletmin:text-6xl">Our Commitment</h2>
+            <p className="phonemin:text-m tabletmin:text-2xl">We understand the thrill of the hunt and the joy of discovery. That's why we meticulously source our inventory, guaranteeing that every item meets the highest standards of quality and authenticity. Whether you're a seasoned collector or new to the world of LaserDiscs, we offer a diverse range of titles that cater to all tastes and interests.</p>
+            </div>
 
-Thank you for visiting <span>Laser Focus</span>. We are excited to help you discover your next treasured find.
+            <img src="/public/LaserDisc-Homepage-Photos/LaserDisc.png" alt="LaserDisc Photo" className="rounded-3xl"/>
+
+            <img src="/public/LaserDisc-Homepage-Photos/Pioneer_CLD-1030_Laserdisc_Player_(Open_tray_with_disc).png" alt="LaserDisc Photo" className="rounded-3xl"/>
+
+          <div>
+            <h2 className="phonemin:py-10 text-4xl tabletmin:text-6xl">Join Our Community</h2>
+            <p className="phonemin:text-m tabletmin:text-2xl">At <span>Laser Focus</span>, we are more than just an online store; we are a community of like-minded individuals who share a deep appreciation for the artistry and nostalgia of LaserDiscs. Connect with us on social media, join our mailing list for exclusive updates, and become part of a vibrant community that celebrates the beauty of collecting rare items.</p>
+            </div>
+          
+            
 </article>
+        <p className="phonemin:text-m p-20 tabletmin:text-2xl">Thank you for visiting <span>Laser Focus</span>. We are excited to help you discover your next treasured find.</p>
       </div>
       <Footer />
     </section>
--- ecommerce-v2-vite/page_components/Movies.jsx
+++ ecommerce-v2-vite/page_components/Movies.jsx
@@ -24,19 +24,19 @@ function Movies() {
       <div className=" text-white">
       <MoviesFetch setMovies={setMovies} setGenres={setGenres} />
       <NavBar />
-      <h1 className="text-center">Movies</h1>
-      <Filter filter={filter} setFilter={setFilter} genres={genres} className="text-black"/>  {/* Pass genres to Filter */}
+      <h1 className="phonemin:text-center py-10 text-4xl tabletmin:text-6xl">Movies</h1>
+      <Filter filter={filter} setFilter={setFilter} genres={genres} />  {/* Pass genres to Filter */}
       <div className="phonemin:grid grid-flow-row-dense grid-cols-2 grid-rows-3 mx-0 relative tabletmin:grid-cols-3">
         {filteredMovies.map((movie, index) => (
-          <div key={index} className="phonemin:text-center m-5">
-            <img src={movie.movie_poster} alt={movie.name} className="phonemin:size-auto "/>
-            <h3>Name: {movie.name}</h3>
-            <h3>Price: {movie.price}</h3>
-            <h3>Bid Amount: {movie.bid}</h3>
-            <h3 className="pl-5 pr-5">Movie Synopsis: {movie.movie_synopsis}</h3>
-            <h3>Release Date: {movie.release_date}</h3>
-            <h3>Genre: {movie.genre_id}</h3>
-            <h3>Availability: {movie.availability}</h3>
+          <div key={index} className="phonemin:text-center m-5 bg-white rounded-3xl space-y-10">
+            <img src={movie.movie_poster} alt={movie.name} className="phonemin:size-auto rounded-3xl"/>
+            <h3 className="text-black phonemin:text-xl tabletmin:text-3xl">Name: {movie.name}</h3>
+            <h3 className="text-black phonemin:text-xl tabletmin:text-3xl">Price: {movie.price}</h3>
+            <h3 className="text-black phonemin:text-xl tabletmin:text-3xl">Bid Amount: {movie.bid}</h3>
+            <h3 className="text-black phonemin:text-xl tabletmin:text-3xl tabletmax:px-10 desktop:px-28">Movie Synopsis: {movie.movie_synopsis}</h3>
+            <h3 className="text-black phonemin:text-xl tabletmin:text-3xl">Release Date: {movie.release_date}</h3>
+            <h3 className="text-black phonemin:text-xl tabletmin:text-3xl">Genre: {movie.genre_id}</h3>
+            <h3 className="text-green-500 phonemin:text-xl tabletmin:text-3xl">Availability: {movie.availability}</h3>
           </div>
         ))}
       </div>
